ae111cf0e1f9fd268d286298c8778957af4c4a7f
Konstantin S. <havendv@gmail.com>
2017-02-04 09:01:57 +1000

    Fixed errors with std::map

--- src/core/CLucene/util/VoidMap.h
+++ src/core/CLucene/util/VoidMap.h
@@ -10,9 +10,12 @@
 #include "Equators.h"
 #include "CLucene/LuceneThreads.h"
 
+#define _CL_HAVE_TR1_UNORDERED_MAP
+#define _CL_HAVE_TR1_UNORDERED_SET
+
 #if defined(_CL_HAVE_TR1_UNORDERED_MAP) && defined(_CL_HAVE_TR1_UNORDERED_SET)
-	#include <tr1/unordered_map>
-	#include <tr1/unordered_set>
+	#include <unordered_map>
+	#include <unordered_set>
 #elif defined(_CL_HAVE_HASH_MAP) && defined(_CL_HAVE_HASH_SET)
 	//hashing is all or nothing!
 	#include <hash_map>
@@ -149,16 +152,16 @@ public:
 
  //a CLSet with CLHashMap traits
 template<typename _kt, typename _vt,
-	typename _Compare,
+	typename CompareType,
 	typename _EqualDummy,
 	typename _KeyDeletor=CL_NS(util)::Deletor::Dummy,
 	typename _ValueDeletor=CL_NS(util)::Deletor::Dummy>
 class CLUCENE_INLINE_EXPORT CLHashMap:public __CLMap<_kt,_vt,
-	CL_NS_STD(map)<_kt,_vt, _Compare>,
+	CL_NS_STD(map)<_kt,_vt, CompareType>,
 	_KeyDeletor,_ValueDeletor>
 {
-	typedef typename CL_NS_STD(map)<_kt,_vt,_Compare> _base;
-	typedef __CLMap<_kt, _vt, CL_NS_STD(map)<_kt,_vt, _Compare>,
+	typedef typename CL_NS_STD(map)<_kt,_vt,CompareType> _base;
+	typedef __CLMap<_kt, _vt, CL_NS_STD(map)<_kt,_vt, CompareType>,
 		_KeyDeletor,_ValueDeletor> _this;
 public:
 	CLHashMap ( const bool deleteKey=false, const bool deleteValue=false )
@@ -255,15 +258,15 @@ public:
 //A collection that contains no duplicates
 //does not guarantee that the order will remain constant over time
 template<typename _kt, typename _vt,
-	typename _Compare,
+	typename CompareType,
 	typename _KeyDeletor=CL_NS(util)::Deletor::Dummy,
 	typename _ValueDeletor=CL_NS(util)::Deletor::Dummy>
 class CLUCENE_INLINE_EXPORT CLSet:public __CLMap<_kt,_vt,
-	CL_NS_STD(map)<_kt,_vt, _Compare>,
+	CL_NS_STD(map)<_kt,_vt, CompareType>,
 	_KeyDeletor,_ValueDeletor>
 {
-	typedef typename CL_NS_STD(map)<_kt,_vt,_Compare> _base;
-	typedef __CLMap<_kt, _vt, CL_NS_STD(map)<_kt,_vt, _Compare>,
+	typedef typename CL_NS_STD(map)<_kt,_vt,CompareType> _base;
+	typedef __CLMap<_kt, _vt, CL_NS_STD(map)<_kt,_vt, CompareType>,
 		_KeyDeletor,_ValueDeletor> _this;
 public:
 	CLSet ( const bool deleteKey=false, const bool deleteValue=false )
@@ -289,7 +292,7 @@ public:
 
 //A collection that can contains duplicates
 template<typename _kt, typename _vt,
-	typename _Compare,
+	typename CompareType,
 	typename _KeyDeletor=CL_NS(util)::Deletor::Dummy,
 	typename _ValueDeletor=CL_NS(util)::Deletor::Dummy>
 class CLUCENE_INLINE_EXPORT CLMultiMap:public __CLMap<_kt,_vt,
